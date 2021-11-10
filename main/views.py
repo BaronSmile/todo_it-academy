a1671db8ce62c6220f32795f0e1749ecb84af6aa
Nurlan <desigantoshka@gmail.com>
2021-11-11 01:54:08 +0700

    added meeting page

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from .models import ToDo
+from .models import ToDo, ToMeet
 
 
 def homepage(request):
@@ -13,3 +13,8 @@ def test(req):
 def test2(req):
     todo_list = ToDo.objects.all()
     return render(req, "test2.html", {"todo_list": todo_list})
+
+
+def toMeet(req):
+    toMeet_list = ToMeet.objects.all()
+    return render(req, 'meeting.html', {'toMeet_list': toMeet_list})
